c7913afdfa27b4ef632ef24af59c2c75e91fc9fb
Qasem Mohammadi <qasimmohammadi520@gmail.com>
2026-02-24 09:48:04 +0430

    user table created

--- src/employees/v2/employees-v2.service.ts
+++ src/employees/v2/employees-v2.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EmployeesService_v2 {
   constructor(private prisma: PrismaService) {}
 
-  findAll(): Promise<
-    Prisma.EmployeeGetPayload<{
-      include: { department: true; position: true };
-    }>[]
-  > {
-    return this.prisma.employee.findMany({
-      include: { department: true, position: true },
-    });
+  findAll() {
+    return this.prisma.employee.findMany();
   }
 }
